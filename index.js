87d703f4e5e901fc70e6e97739a73b7d76a80d13
ferdinardrain <your_github_ferdinardrain@gmail.com>
2026-01-14 13:07:46 +0000

    Increase payload limit to 10mb for image uploads

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 
 
